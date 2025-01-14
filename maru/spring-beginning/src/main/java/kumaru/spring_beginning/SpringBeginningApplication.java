ad49d145cfd2d7126556af6a3d0fceb94e04b57d
maru <p_sso_0@ewhain.net>
2025-01-14 21:43:42 +0900

    #섹션2: 프로젝트 환경설정

---
+++ maru/spring-beginning/src/main/java/kumaru/spring_beginning/SpringBeginningApplication.java
@@ -0,0 +1,13 @@
+package kumaru.spring_beginning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeginningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeginningApplication.class, args);
+	}
+
+}
---
+++ maru/spring-beginning/src/test/java/kumaru/spring_beginning/SpringBeginningApplicationTests.java
@@ -0,0 +1,13 @@
+package kumaru.spring_beginning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeginningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

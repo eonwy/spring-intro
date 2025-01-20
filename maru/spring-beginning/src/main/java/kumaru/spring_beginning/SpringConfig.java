21fbff4a7bafbeec08f71f9924e8438efcd639b0
maru <p_sso_0@ewhain.net>
2025-01-21 01:17:28 +0900

    #섹션7: 순수 JDBC

--- maru/spring-beginning/src/main/java/kumaru/spring_beginning/SpringConfig.java
+++ maru/spring-beginning/src/main/java/kumaru/spring_beginning/SpringConfig.java
@@ -1,13 +1,26 @@
 package kumaru.spring_beginning;
 
+import kumaru.spring_beginning.repository.JdbcMemberRepository;
 import kumaru.spring_beginning.repository.MemberRepository;
 import kumaru.spring_beginning.repository.MemoryMemberRepository;
 import kumaru.spring_beginning.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+//    @Autowired  DataSource dataSource;
+
+    private  DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +29,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
